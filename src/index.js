784464c6e682c262d07e2bcfa6063c2f57b2111a
burnsred94 <67732974+burnsred94@users.noreply.github.com>
2021-01-28 14:57:19 +0300

    init: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strArray = str.split(''),
+        brackets = '';
+    console.log(str);
+    bracketsConfig = bracketsConfig.map(item => item = item.join(''));
+    console.log(bracketsConfig);
+    while (
+        bracketsConfig.some((item, index) => {
+            if (strArray.join('').includes(item)) brackets = item;
+            return strArray.join('').includes(item);
+        })) {
+        strArray.splice(strArray.join('').indexOf(brackets), 2);
+    };
+
+    console.log(strArray.join(''));
+    return strArray.join('') == '' ? true : false;
 }
